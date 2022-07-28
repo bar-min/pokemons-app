58b306cc75f723271cc272d9beb8e90313d37c0b
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-07-28 20:47:58 +0400

    add poke-ball component and evolution.scss

---
+++ src/components/PokeBall.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="pokeball" @click="likePokemon(pokemon)" v-if="!pokemon.liked"></div>
+   <div class="pokeball pokeball_active" @click="removePokemon({ pokemon, render: false })" v-else></div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  props: ['pokemon'],
+
+  methods: {
+    ...mapActions(['likePokemon', 'removePokemon'])
+  }
+}
+</script>
+
+<style>
+
+</style>
